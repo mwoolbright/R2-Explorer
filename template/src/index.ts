7c6907e871a1edb51b98366a4ad09bf42208e310
mwoolbright <44710742+mwoolbright@users.noreply.github.com>
2025-11-09 18:58:09 -0600

    trying again

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,7 +3,7 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
 	readonly: false,
-	cfAccessTeamName: 'your-team-name' // set this to your actual team name
+	cfAccessTeamName: 'woolbright.cloudflareaccess.com', // set this to your actual team name
 	// basicAuth: {
 	//   username: 'username',
 	//   password: 'password'
