937b782dfd8a613cc006dd93601959218f4bcf56
mwoolbright <44710742+mwoolbright@users.noreply.github.com>
2025-11-09 18:51:27 -0600

    adding cloudflare teams authentication

--- template/src/index.ts
+++ template/src/index.ts
@@ -2,7 +2,8 @@ import { R2Explorer } from "r2-explorer";
 
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
-	readonly: true,
+	readonly: false,
+	cfAccessTeamName: 'your-team-name' // set this to your actual team name
 	// basicAuth: {
 	//   username: 'username',
 	//   password: 'password'
